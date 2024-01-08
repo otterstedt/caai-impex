ebb5c33ac284d73266ef1e51d82e22bbb61c4a66
Michael Otterstedt <michael.otterstedt@sap.com>
2024-01-08 17:02:41 -0500

    iterate rows and convert to json and print

--- cimpex.py
+++ cimpex.py
@@ -5,10 +5,22 @@
 import openpyxl
 from openpyxl import Workbook
 from openpyxl import load_workbook
+import json
 
 wb = load_workbook(filename="./files/alumni_list_gbg_20231020.xlsx")
 
 print("Sheets")
 for sheet in wb:
   print("'" + sheet.title + "'")
+  full_list=[]
+
+  for row in sheet.iter_rows(min_row=2):
+    drow={}
+    for cell in row:
+      if sheet.cell(row=1, column=cell.column).value and cell.value:
+        drow[sheet.cell(row=1, column=cell.column).value]=str(cell.value)
+    full_list.append(drow)
+    #print(json.dumps(drow, indent=2))
+
+print(json.dumps(full_list, indent=2))
 
